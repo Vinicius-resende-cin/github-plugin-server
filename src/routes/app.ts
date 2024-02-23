741e5b7434e3ea2d10d023ef8805773d02e6c662
vinicius <vrb@cin.ufpe.br>
2024-02-23 14:25:45 -0300

    Add PUT and DELETE routes for analysis

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -44,7 +44,31 @@ app.post("/analysis", async (req, res) => {
   res.send(createdAnalysis);
 });
 
-// app.put("/analysis", (req, res) => {});
-// app.delete("/analysis", (req, res) => {});
+app.put("/analysis", async (req, res) => {
+  if (!req.body.analysis) return res.status(400).send("Bad request: analysis not provided");
+
+  const analysis: IAnalysisOutput = req.body.analysis;
+
+  const updatedAnalysis = await analysisController
+    .updateAnalysis(analysis)
+    .then((analysis) => JSON.stringify(analysis));
+
+  res.send(updatedAnalysis);
+});
+
+app.delete("/analysis", async (req, res) => {
+  const owner = req.query.owner as string;
+  const repo = req.query.repo as string;
+  const pull_number = req.query.pull_number as string;
+
+  if (!owner) return res.status(400).send("Bad request: owner not provided");
+  if (pull_number && !repo) return res.status(400).send("Bad request: repo not provided");
+
+  await analysisController
+    .deleteAnalysis(repo, owner, parseInt(pull_number))
+    .then((analysis) => JSON.stringify(analysis));
+
+  return res.send();
+});
 
 export default app;
