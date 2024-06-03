5f48c304eb0cea12feb003f2331ea11acf234626
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-06-03 12:41:22 -0300

    Add cors

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import AnalysisController from "../controllers/analysisController";
 import { IAnalysisOutput } from "../models/AnalysisOutput";
 import mongoose from "mongoose";
@@ -17,6 +18,7 @@ const analysisController = new AnalysisController();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/analysis", async (req, res) => {
   const owner = req.query.owner as string;
