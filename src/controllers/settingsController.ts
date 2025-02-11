3f301051751f05c8b8213d0e34ae41cafcc622a0
vinicius <vrb@cin.ufpe.br>
2025-02-10 22:31:54 -0300

    Add getSettings method to controller

--- src/controllers/settingsController.ts
+++ src/controllers/settingsController.ts
@@ -6,11 +6,16 @@ let settingsDataRepository: SettingsDataRepository =
   persistenceType === "mongo" ? new SettingsDataMongoRepository() : new SettingsDataMongoRepository();
 
 interface IASettingsController {
+  getSettings: (repository: string, owner: string, pull_number: number) => Promise<SettingsData>;
   createSettings: (settings: IASettingsData) => Promise<SettingsData>;
   updateSettings: (settings: IASettingsData) => Promise<SettingsData>;
 }
 
 class SettingsController implements IASettingsController {
+  async getSettings(repository: string, owner: string, pull_number: number): Promise<SettingsData> {
+    return await settingsDataRepository.getSettingsData(repository, owner, pull_number);
+  }
+
   async createSettings(settings: IASettingsData): Promise<SettingsData> {
     const newSettings = new SettingsData(settings);
     return await settingsDataRepository.createSettingsData(newSettings);
@@ -20,7 +25,6 @@ class SettingsController implements IASettingsController {
     const newSettings = new SettingsData(settings);
     return await settingsDataRepository.updateSettingsData(newSettings);
   }
-
 }
 
 export default SettingsController;
